a670f861c5edafa0c28044dc8dd81bb52bf34b32
Pavaresorn Malai <61514399+Varkaria@users.noreply.github.com>
2023-06-24 11:28:44 +0700

    fix: import error

--- apps/video/src/modules/Root.tsx
+++ apps/video/src/modules/Root.tsx
@@ -1,18 +1,18 @@
-import {Composition} from 'remotion';
-import {MyComposition} from './Composition';
-import '../styles/index.css';
+import { Composition } from "remotion";
+import { MyComposition } from "./Composition";
+import "../styles/style.css";
 
 export const RemotionRoot: React.FC = () => {
-	return (
-		<>
-			<Composition
-				id="Main"
-				component={MyComposition}
-				durationInFrames={240}
-				fps={30}
-				width={360}
-				height={360}
-			/>
-		</>
-	);
+  return (
+    <>
+      <Composition
+        id="Main"
+        component={MyComposition}
+        durationInFrames={240}
+        fps={30}
+        width={360}
+        height={360}
+      />
+    </>
+  );
 };

4ab37f58878ebab7598da092ad4d3817be339b91
Gusb3ll <Kitpipat.0647@gmail.com>
2023-06-24 12:36:46 +0700

    feat: random voice

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,16 +1,21 @@
 import express from 'express'
 
 import { birthdayWishes } from './constant'
+import { generateVoice } from './utils'
 
 //
 (() => {
 const app = express()
 app.use(express.json())
 
-app.post('/random', (req, res) => {
+app.post('/random', async (req, res) => {
 const name = req.body?.name
+const gender = req.body?.gender
 if (!name) {
   res.status(400).json({ statusCode: 400, message: 'body "name" is required' })
+}
+if (!gender) {
+  res.status(400).json({ statusCode: 400, message: 'body "gender" is required [MALE, FEMALE]' })
 }
 const randomMessage1 = birthdayWishes[Math.floor(Math.random() * birthdayWishes.length)].replace('_NAME_', name)
 const randomMessage2 = birthdayWishes[Math.floor(Math.random() * birthdayWishes.length)].replace('_NAME_', name)
@@ -18,7 +23,9 @@ const randomMessage3 = birthdayWishes[Math.floor(Math.random() * birthdayWishes.
 const randomMessage4 = birthdayWishes[Math.floor(Math.random() * birthdayWishes.length)].replace('_NAME_', name)
 const randomMessage = `${randomMessage1} ` + `${randomMessage2} ` + `${randomMessage3} ` + `${randomMessage4}`
 
-res.status(200).json({ statusCode: 200, data: { message: randomMessage } })
+const voice = await generateVoice('ฮิฟูมินด็อตแอป เว็ปไซต์หาหนังโป๊เด็ก cunny ทับ c ทับ 9', gender)
+
+res.status(200).json({ statusCode: 200, data: { message: randomMessage, voice } })
 })
 
 app.listen(4000, 'localhost', () => {
---
+++ apps/server/src/utils.ts
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+export const generateVoice = async (message: string, gender: 'MALE' | 'FEMALE') => {
+  const { data } = await axios.post(process.env.VOICE_ROTATE_URL, {
+    audioFormat: "ogg",
+    paragraphChunks: [message],
+    voiceParams: { 
+      name: gender === 'MALE' ? "Niwat" : "Achara",
+      engine: "azure",
+      languageCode: "th-TH"
+    }
+  })
+  const audioStream: string = data.audioStream
+
+  return audioStream
+}

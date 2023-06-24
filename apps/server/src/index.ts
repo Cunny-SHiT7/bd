22b2cb7030fa4b2c68f0b5d8e6a07776565f6908
Gusb3ll <Kitpipat.0647@gmail.com>
2023-06-24 11:46:56 +0700

    feat: randomMessage

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,22 +1,31 @@
 import http from 'http'
+import axios from 'axios'
 import express from 'express'
-import { ChatGPTUnofficialProxyAPI } from 'chatgpt'
+
+import { birthdayWishes } from './constant'
+
 //
 (async () => {
-  const app = express()
-  const api = new ChatGPTUnofficialProxyAPI({
-    accessToken: process.env.OPENAI_ACCESS_TOKEN,
-    apiReverseProxyUrl: 'https://your-example-server.com/api/conversation'
-  })
-
+const app = express()
 app.use(express.json())
 
 app.get('/', (_req, res) => { 
   res.send('OK')
 })
 
-app.post('/randomMessage', (_req, res) => {
 
+app.post('/randomMessage', async (req, res) => {
+const { name } = req.body
+if (!name) {
+  return res.status(400).json({ statusCode: 400, message: 'body "name" is required' })
+}
+const randomMessage1 = birthdayWishes[Math.floor(Math.random() * birthdayWishes.length)].replace('_NAME_', name)
+const randomMessage2 = birthdayWishes[Math.floor(Math.random() * birthdayWishes.length)].replace('_NAME_', name)
+const randomMessage3 = birthdayWishes[Math.floor(Math.random() * birthdayWishes.length)].replace('_NAME_', name)
+const randomMessage4 = birthdayWishes[Math.floor(Math.random() * birthdayWishes.length)].replace('_NAME_', name)
+const randomMessage = `${randomMessage1} ` + `${randomMessage2} ` + `${randomMessage3} ` + `${randomMessage4}`
+
+return res.status(200).json({ statusCode: 200, data: randomMessage })
 })
 
 http.createServer(app).listen(4000, 'localhost', () => {

be68ece6c9c030ef448c3b03762fdd09518dc017
Gusb3ll <Kitpipat.0647@gmail.com>
2023-06-24 12:07:41 +0700

    fix: path

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -7,14 +7,10 @@ import { birthdayWishes } from './constant'
 const app = express()
 app.use(express.json())
 
-app.get('/', (_req, res) => { 
-  res.send('OK')
-})
-
-app.post('/randomMessage', async (req, res) => {
-const { name } = req.body
+app.post('/random', (req, res) => {
+const name = req.body?.name
 if (!name) {
-  return res.status(400).json({ statusCode: 400, message: 'body "name" is required' })
+  res.status(400).json({ statusCode: 400, message: 'body "name" is required' })
 }
 const randomMessage1 = birthdayWishes[Math.floor(Math.random() * birthdayWishes.length)].replace('_NAME_', name)
 const randomMessage2 = birthdayWishes[Math.floor(Math.random() * birthdayWishes.length)].replace('_NAME_', name)
@@ -22,7 +18,7 @@ const randomMessage3 = birthdayWishes[Math.floor(Math.random() * birthdayWishes.
 const randomMessage4 = birthdayWishes[Math.floor(Math.random() * birthdayWishes.length)].replace('_NAME_', name)
 const randomMessage = `${randomMessage1} ` + `${randomMessage2} ` + `${randomMessage3} ` + `${randomMessage4}`
 
-return res.status(200).json({ statusCode: 200, data: { message: randomMessage } })
+res.status(200).json({ statusCode: 200, data: { message: randomMessage } })
 })
 
 app.listen(4000, 'localhost', () => {

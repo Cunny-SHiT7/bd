4046c72c3fefd71b943cf03820af88656a2b4a06
Gusb3ll <Kitpipat.0647@gmail.com>
2023-06-25 16:47:51 +0700

    dai mai

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -57,6 +57,7 @@ import { createRender, getRender, updateRender } from './db'
       const bundled = await bundle(path.join(__dirname, '../remotion/index.ts'), () => { return }, {
         outDir: path.join(__dirname, '../out'),
       })
+      fs.writeFileSync(path.join(__dirname, '../out/bundled.txt'), bundled)
       console.log('FILE BUNDLED')
       // Extract all the compositions you have defined in your project
       // from the webpack bundle.

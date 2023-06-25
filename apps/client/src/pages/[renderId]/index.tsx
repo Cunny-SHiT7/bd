b10d4aa88f00af36f7383fe9776fd0df9162f6f9
Supratouch <supratouchgussii@gmail.com>
2023-06-25 11:44:25 +0700

    fix: stop fetching

--- apps/client/src/pages/[renderId]/index.tsx
+++ apps/client/src/pages/[renderId]/index.tsx
@@ -1,15 +1,25 @@
 import { useParams } from 'react-router'
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 const RenderInformationPage = () => {
+
+  const [stopFetching, setStopFetching] = useState(false)
+
   const params = useParams<{ renderId: string }>()
   const data = useQuery({
     queryKey: ['render', params.renderId],
     queryFn: async () =>
       await axios.get(`http://localhost:4000/getRender/${params.renderId}`),
     refetchInterval: 1000,
+    enabled: !stopFetching
   })
+
+  useEffect(() => {
+    if (!data.data?.data.url) return
+    setStopFetching(true)
+  }, [data.data?.data])
 
   if (data.isLoading) return <div>Loading...</div>
 
@@ -21,7 +31,7 @@ const RenderInformationPage = () => {
           <>
             <div>
               <img src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExcmE2MTNsNHM2MG51bDE0aGIzMHZ5dzdhbmtram9oMTJmYjZ0ZjV4bCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o7bu3XilJ5BOiSGic/giphy.gif" />
-              กำลังทำวิดีโอให้ไอ่ควาย
+              กำลังทำวิดีโอให้ใจเยนๆ
               <p>{data.data?.data.process * 100}%</p>
             </div>
           </>
